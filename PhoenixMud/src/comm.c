ee7f91967bf50d0884974bb05534e5abae551801
Xandrian McMacken <xmcmacken@gmail.com>
2025-09-29 10:06:35 -0700

    Capitalize sentences in act()
    Capitalize sentences in perform_act()
    Accounts for color escape codes and & color codes.

--- PhoenixMud/src/comm.c
+++ PhoenixMud/src/comm.c
@@ -2849,7 +2849,23 @@ void perform_act(char *orig, struct char_data *ch, struct obj_data *obj,
    *(++buf) = '\0'; 
  
    if (to->desc)
-      SEND_TO_Q(to->desc,"%s",CAP(lbuf));
+   {
+	  /* Capitalize the sentence, account for color escape codes - Nomi 9/29/25 */
+	  /* Hope I didn't break anything. */
+	  if (*lbuf == '\x1B')
+	  {
+	     if ((strlen(lbuf) > 4) && (*(lbuf + 4) == '\x1B'))
+	        CAP(lbuf + 9);
+	     else
+		    CAP(lbuf + 5);
+	  }
+	  else if (*lbuf == '&')
+		  CAP(lbuf + 2);
+	  else
+		  CAP(lbuf);
+	  
+      SEND_TO_Q(to->desc,"%s", lbuf);
+   }
    if (MOBTrigger)
       mprog_act_trigger(lbuf, to, ch, obj, vict_obj);
    if (IS_NPC(to) && dg_act_check)
